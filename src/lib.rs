2ae15779a64172dac67c3dc4b5cf5aab27221570
UE2020 <ttmmiizz101@gmail.com>
2022-03-26 15:18:40 -0400

    Add clipboard module

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,32 @@ pub use graphics::*;
 
 use std::ffi::CString;
 
+pub use gl;
+
+pub mod clipboard {
+    use crate::graphics::*;
+
+    use clipboard::*;
+
+    static mut CLIPBOARD_CTX: Option<ClipboardContext> = None;
+
+    pub fn ctx() -> &'static mut ClipboardContext {
+        unsafe { CLIPBOARD_CTX.as_mut().unwrap_or_else(|| {
+            let ctx = ClipboardContext::new().unwrap();
+            CLIPBOARD_CTX = Some(ctx);
+            CLIPBOARD_CTX.as_mut().unwrap()
+        }) }
+    }
+
+    pub fn get(_ctx: &mut Context) -> Option<String> {
+        ctx().get_contents().ok()
+    }
+
+    pub fn set(_ctx: &mut Context, data: &str) {
+        ctx().set_contents(data.to_string()).unwrap();
+    }
+}
+
 pub mod date {
     #[cfg(not(target_arch = "wasm32"))]
     pub fn now() -> f64 {
